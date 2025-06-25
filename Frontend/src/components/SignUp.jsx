ef4a80562ce2b777c811055906157289acdd1ace
kshitij <kshitijbarman1234@gmail.com>
2025-06-25 22:58:57 +0530

    update

--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -64,31 +64,3 @@ const SignUpForm = () => {
 };
 export default SignUpForm;
 
-
-
-// <div className='h-screen w-full flex items-center justify-center bg-amber=200'>
-//         <div className='hidden w-1/2 h-full bg-amber-800 lg:block'>
-//             <div className='flex gap-x-5 pt-5 pl-5'>
-//             <img src='/savan-logo.png' className='w-10'/>
-//             <h2 className='text-xl font-semi-bold text-white'>jiosaavn</h2>
-//             </div>
-//             <img src='https://staticweb6.jiosaavn.com/web6/jioindw/dist/1741237096/_i/artist/KaurB.png' className='w-[25rem]  mx-auto mt-15'/>
-//             <h1 className='text-center text-3xl font-bold text-white'>All Your Music.</h1>
-//             <h1 className='text-center text-3xl font-bold text-gray-100'>Anytime, anywhere.</h1>
-//         </div>
-
-
-//         <div className='w-1/2 '>
-//         <div className='bg-gray-200 w-[22rem] min-h-[22rem] mx-auto  rounded-2xl'>
-//            <h1 className='text-center text-3xl font-semibold pt-5 pb-6'>Login</h1>
-//            <form onSubmit={handleSubmit}>
-//               <div className='px-5 space-y-3 '>
-//               <input type='email' name='email' value={formData.email} placeholder='Enter Your Email' required onChange={handleChange} className='border-b w-full py-2 px-2 outline-none' ></input>
-//               <input type='password' name='password' value={formData.password}  placeholder='Enter Your password' required onChange={handleChange} className='border-b w-full py-2 px-2 outline-none' ></input>
-//               <button type='submit' className='bg-blue-600 w-full mt-3 py-2 text-white hover:bg-blue-700 font-semibold'>Login</button>
-//               </div>
-//            </form>
-//            <p className="text-center mt-3">Don't have an account? <span className="text-blue-500"><NavLink to='/sign-up'>Sign up</NavLink></span></p>
-//        </div>
-//         </div>
-//       </div>
--- Frontend/src/components/Slidebar.jsx
+++ Frontend/src/components/Slidebar.jsx
@@ -1,135 +1,41 @@
-// import React from "react";
-// import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-
-
-// const Slidebar = () => {
-//   return (
-//     <Sidebar >
-//       <Menu className="pl-5 text-sm ">
-//         <h2 className=" text-center py-2">BROWSER</h2>
-//         <MenuItem> New Releases </MenuItem>
-//         <MenuItem> Top Charts </MenuItem>
-//         <MenuItem> Top Playlists </MenuItem>
-//         <MenuItem> Podcasts </MenuItem>
-//         <MenuItem> Top Artists </MenuItem>
-//         <MenuItem> Radio </MenuItem>
-//         <h2 className="py-2 text-center">Library</h2>
-//         <MenuItem> History </MenuItem>
-//         <MenuItem> Liked Songs </MenuItem>
-//         <MenuItem>  Albums </MenuItem>
-//         <MenuItem> Podcasts </MenuItem>
-//         <MenuItem> Artists </MenuItem>
-//       </Menu>
-//     </Sidebar>
-//   );
-// };
-
-// export default Slidebar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-
-// const Slidebar = () => {
-//   return (
-//     <Sidebar className="h-[80vh]   rounded-lg">
-//       <Menu className="pl-5 text-sm">
-//         <h2 className="text-center py-2 text-gray-700">BROWSER</h2>
-//         <MenuItem> New Releases </MenuItem>
-//         <MenuItem> Top Charts </MenuItem>
-//         <MenuItem> Top Playlists </MenuItem>
-//         <MenuItem> Podcasts </MenuItem>
-//         <MenuItem> Top Artists </MenuItem>
-//         <MenuItem> Radio </MenuItem>
-
-//         <h2 className="py-2 text-center text-gray-700">LIBRARY</h2>
-//         <MenuItem> History </MenuItem>
-//         <MenuItem> Liked Songs </MenuItem>
-//         <MenuItem> Albums </MenuItem>
-//         <MenuItem> Podcasts </MenuItem>
-//         <MenuItem> Artists </MenuItem>
-//       </Menu>
-//     </Sidebar>
-//   );
-// };
-
-// export default Slidebar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../redux/themeSlice";
 import { NavLink } from "react-router-dom";
-import PlayListPage from './../pages/PlayListPage';
-
 
 const Slidebar = () => {
-   const darkMode = useSelector((state) => state.theme.darkMode); 
-    const dispatch = useDispatch();
-  
-  return (
-    <Sidebar className='h-[57vh] rounded-lg pt-20'>
-      {/* <Menu className={`pl-5 text-sm space-y-1 ${darkMode ? 'bg-[#f5f5f5]' : 'bg-black text-white'}`} > */}
-      <Menu className={`pl-5 text-sm space-y-1 ${darkMode ? 'bg-[#f5f5f5]' : 'bg-black text-white'}`} >
-        {/* <h2 className={`text-center py-1 pt-20 text-gray-800 ${darkMode ? 'bg-[#f5f5f5]' : 'bg-black text-white'}`}>BROWSER</h2>
-        <MenuItem className="py-1"> New Releases </MenuItem>
-        <MenuItem className="py-1"> Top Charts </MenuItem>
-        <MenuItem className="py-1"> Top Playlists </MenuItem>
-        <MenuItem className="py-1"> Podcasts </MenuItem>
-        <MenuItem className="py-1"> Top Artists </MenuItem>
-        <MenuItem className="py-1"> Radio </MenuItem> */}
+  const darkMode = useSelector((state) => state.theme.darkMode);
+  const dispatch = useDispatch();
 
-        <h2 className={`py-1 text-center text-gray-800 ${darkMode ? 'bg-[#f5f5f5]' : 'bg-black text-white'}`}>LIBRARY</h2>
-        <NavLink to='/artist'> <MenuItem className="py-1"> Artists </MenuItem></NavLink>
-        <NavLink to='/playlist'> <MenuItem className="py-1" > Liked Songs </MenuItem></NavLink>
-        <NavLink to='/album'><MenuItem className="py-1"> Albums </MenuItem></NavLink>
-        <MenuItem className="py-1"> Podcasts </MenuItem>
-        <MenuItem className="py-1"> History </MenuItem>
+  return (
+    <Sidebar className="h-[57vh] rounded-lg pt-20">
+      <Menu
+        className={`pl-5 text-sm space-y-1 ${
+          darkMode ? "bg-[#f5f5f5]" : "bg-black text-white"
+        }`}
+      >
+        <h2
+          className={`py-1 text-center text-gray-800 ${
+            darkMode ? "bg-[#f5f5f5]" : "bg-black text-white"
+          }`}
+        >
+          LIBRARY
+        </h2>
+
+        {/* ✅ Fixed: pass NavLink as component to MenuItem */}
+        <MenuItem component={<NavLink to="/artist" />} className="py-1">
+          Artists
+        </MenuItem>
+        <MenuItem component={<NavLink to="/playlist" />} className="py-1">
+          Liked Songs
+        </MenuItem>
+        <MenuItem component={<NavLink to="/album" />} className="py-1">
+          Albums
+        </MenuItem>
+
+        <MenuItem className="py-1">Podcasts</MenuItem>
+        <MenuItem className="py-1">History</MenuItem>
       </Menu>
     </Sidebar>
   );
